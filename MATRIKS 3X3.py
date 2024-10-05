3489b293363ecc029e770e5d39167c385f51f624
Afifaftawi <afifaftawi07@gmail.com>
2024-10-05 19:42:00 +0800

    Create MATRIKS 3X3.py

---
+++ MATRIKS 3X3.py
@@ -0,0 +1,14 @@
+def multiply_matriks(A,B):
+    if len(A[0]) !=len(B):
+        return "matriks tidak dapat dikalikan"
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+
+    return result
+
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+print(multiply_matriks(A,B))
